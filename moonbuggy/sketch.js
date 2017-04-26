1f1847fa17f66bf4e290c782b4c1ed4017f24172
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2017-04-26 23:37:57 +0800

    Update sketch.js

--- moonbuggy/sketch.js
+++ moonbuggy/sketch.js
@@ -1,10 +1,19 @@
-var img;  
+var bike;  
+var bikeImg; 
+function preload(){
+  bikeImg = loadImage("motorcycleman.jpg"); 
+}
 function setup(){
-  createCanvas(600,600); 
-  img = loadImage("Images/motorcycleman.jpg");  
+  createCanvas(400,400);  
+  bike = createSprite(width/2,height/2); 
+  bike.addImage(bikeImg);  
 }
 function draw(){
-  background(240); 
-  image(img,0,0);  
-  
+  background(255); 
+  bike.position.x = mouseX; 
+  bike.position.y = mouseY;  
+  if (mouseIsPressed){
+    bike.rotation += 2; 
+  }
+  drawSprites(); 
 }
