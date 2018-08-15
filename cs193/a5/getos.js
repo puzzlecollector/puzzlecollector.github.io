cb6dc288d6806082756b494f24afaff2e111c043
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-08-15 14:47:54 -0700

    Add files via upload

---
+++ cs193/a5/getos.js
@@ -0,0 +1,7 @@
+/*
+Write a short program that retrieves and prints the 
+type of OS you are using by the "platform" method from the 
+"os" module.  
+*/ 
+const os = require("os"); 
+console.log(os.platform()); // darwin is printed  
